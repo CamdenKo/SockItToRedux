21a27ca761fe457b4894045ac4d7d7d473eca26f
Camden Ko <camdenko@gmail.com>
2017-12-01 15:20:44 -0600

    backend socketReducer

--- backEnd/backEnd.js
+++ backEnd/backEnd.js
@@ -5,9 +5,49 @@ const {
   requestSocketNamer,
 } = require('../utilities/utilities')
 
-const socketAjaxReducer = (socket, socketName) => {
+const defaultState = {
+  requestPending: false,
+  requestData: null,
+}
 
+const socketReducerBackEnd = (socket, socketName) => {
+  const uppName = uppify(socketName)
+  const REQUEST = `REQUEST_${uppName}`
+  const REQUEST_FULFILLED = `REQUEST_FULFILLED_${uppName}`
   return {
-
+    actionCreators: {
+      [actionCreatorNamer('request', socketName)]: data => ({ type: REQUEST, data }),
+      [actionCreatorNamer('requestFulfilled', socketName)]: () => ({ type: REQUEST_FULFILLED }),
+    },
+    reducer: (state = defaultState, action) => {
+      switch (action.type) {
+        case REQUEST:
+          return { ...state, requestPending: true, data: action.data }
+        case REQUEST_FULFILLED:
+          return { ...state, requestPending: false, data: null }
+        default:
+          return state
+      }
+    },
+    success: data =>
+      (dispatch) => {
+        socket.emit(actionCreatorNamer('success', socketName), data)
+        dispatch(this.actionCreators[actionCreatorNamer('requestFulfilled', socketName)]())
+      },
+    error: err =>
+      (dispatch) => {
+        socket.emit(actionCreatorNamer('error', socketName), err)
+        dispatch(this.actionCreators[actionCreatorNamer('requestFulfilled', socketName)]())
+      },
+    socketSubscriber: (store) => {
+      const { dispatch } = store
+      socket.on(requestSocketNamer(socketName), (data) => {
+        dispatch(this.actionCreators[actionCreatorNamer('request')](data))
+      })
+    },
   }
+}
+
+module.exports = {
+  socketReducerBackEnd,
 }
